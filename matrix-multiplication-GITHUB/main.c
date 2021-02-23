423504465464901156d01751aeacd0d9fc831574
Noah-Huff <noah.b.huff@gmail.com>
2021-02-23 10:26:05 -0600

    Initial Commit

---
+++ matrix-multiplication-GITHUB/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  matrix-multiplication-GITHUB
+//
+//  Created by Noah Huff on 2/23/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
